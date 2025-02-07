438ff8c152d15580584fdb29758570cea61975b9
sumanth222 <jsendeavour787@gmail.com>
2025-02-07 21:53:50 +0530

    Added resposne header

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,13 @@ require('dotenv').config();
 app.use(express.json());
 app.use(cors({ origin: "https://muscle-makers-fe.vercel.app" }));
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://muscle-makers-fe.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 const app = express();
 const PORT = process.env.PORT || 10000;
 const pool = new Pool({
