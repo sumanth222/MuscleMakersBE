10b4caac818536ac02b5c64591deab8212603ae1
sumanth222 <jsendeavour787@gmail.com>
2025-02-07 21:36:55 +0530

    Updated port

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
@@ -17,8 +17,8 @@ const pool = new Pool({
 app.use(cors());
 app.use(express.json());
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
 
 app.get('/', (req, res) => {
